c587d799a7b68604db49bf3914601690e981712c
Dmitry Troshenkov <troshenkov.d@gmail.com>
2018-12-01 14:16:53 -0800

    Edit email-valid.sh

--- regex/email-valid.sh
+++ regex/email-valid.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-email='troshenkov.d@gmail..com'
+email=troshenkov.d@gmail.com
 
-_RFC822="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
+pattern='^[[:alnum:]._%+-]+@[[:alnum:]._+-]+.[[:alpha:]]{2,4}$'
 
-if [[ $email =~ $_RFC822 ]]; then
+if [[ $email =~ $pattern ]]; then
 
-#if [[ $email =~ ^[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:].]{2,4}$ ]]; then
-
-echo good
+    echo good
 
 else
 
