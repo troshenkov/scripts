6b93b03fec1c2e3c0964b7d3e76b1e98347959bd
Dmitry Troshenkov <troshenkov.d@gmail.com>
2025-04-04 19:41:31 -0700

    Refactor bad IP blocking script: modularize, improve error handling, and readability

--- iptables/badips.sh
+++ iptables/badips.sh
@@ -1,38 +1,60 @@
 #!/bin/bash
-# Script for blocking IPs which have been reported to www.badips.com
-# Usage: Just execute by e.g. cron every day
-# ---------------------------
- 
-_ipt=/sbin/iptables    # Location of iptables (might be correct)
-_input=badips.db       # Name of database (will be downloaded with this name)
-_pub_if=eth0           # Device which is connected to the internet (ex. $ifconfig for that)
-_droplist=droplist     # Name of chain in iptables (Only change this if you have already a chain with this name)
-#_level=5               # Blog level: not so bad/false report (0) over confirmed bad (3) to quite aggressive (5) (see www.badips.com for that)
-_service=any           # Logged service (see www.badips.com for that)
-_age=2m
+# ===================================================================
+# Script for Blocking IPs Reported on www.badips.com
+# ===================================================================
+#
+# This script blocks IPs that have been reported to www.badips.com.
+# It downloads a list of bad IPs and configures iptables to block them.
+#
+# Usage: Run this script daily, e.g., via cron.
+#
+# Author: Dmitry Troshenkov (troshenkov.d@gmail.com)
+# ===================================================================
 
-# Get the bad IPs
-#wget -qO- http://www.badips.com/get/list/${_service}/$_level > $_input || { echo "$0: Unable to download ip list."; exit 1; }
-wget -qO- http://www.badips.com/get/list/${_service}/3?age=${_age} > $_input || { echo "$0: Unable to download ip list."; exit 1; }
+# Define variables
+_ipt=/sbin/iptables                # Location of iptables
+_input=badips.db                   # Name of database (will be downloaded with this name)
+_pub_if=eth0                       # Public interface (e.g., eth0 or enp0s3)
+_droplist=droplist                  # Name of chain in iptables (can be modified)
+_service=any                        # Logged service (e.g., "any" or "ssh")
+_age=2m                             # Age filter for IPs (e.g., 2 months)
 
-### Setup our black list ###
-# First flush it
-$_ipt --flush $_droplist
-# Create a new chain
-$_ipt -N $_droplist
- 
-# Filter out comments and blank lines
-# store each ip in $ip
-for ip in `cat $_input`
-do
-    # Append everything to $_droplist
-    $_ipt -A $_droplist -i ${_pub_if} -s $ip -j LOG --log-prefix "Drop Bad IP List"
-    $_ipt -A $_droplist -i ${_pub_if} -s $ip -j DROP
-done
- 
-# Finally, insert or append our black list
-$_ipt -I INPUT -j $_droplist
-$_ipt -I OUTPUT -j $_droplist
-$_ipt -I FORWARD -j $_droplist
- 
+# Function to download the bad IPs list from badips.com
+download_bad_ips() {
+    echo "Downloading bad IPs list from www.badips.com..."
+    wget -qO- "http://www.badips.com/get/list/${_service}/3?age=${_age}" > $_input || { echo "$0: Unable to download IP list."; exit 1; }
+}
+
+# Function to set up the blacklist in iptables
+setup_blacklist() {
+    echo "Setting up the blacklist in iptables..."
+    # Flush existing chain
+    $_ipt --flush $_droplist
+    # Create a new chain
+    $_ipt -N $_droplist
+
+    # Filter out comments and blank lines, then add IPs to the blacklist
+    while read -r ip; do
+        if [[ -n "$ip" && "$ip" != \#* ]]; then  # Skip comments and empty lines
+            $_ipt -A $_droplist -i ${_pub_if} -s "$ip" -j LOG --log-prefix "Drop Bad IP List"
+            $_ipt -A $_droplist -i ${_pub_if} -s "$ip" -j DROP
+        fi
+    done < $_input
+}
+
+# Function to apply the blacklist to iptables
+apply_blacklist() {
+    echo "Applying the blacklist to iptables..."
+    $_ipt -I INPUT -j $_droplist
+    $_ipt -I OUTPUT -j $_droplist
+    $_ipt -I FORWARD -j $_droplist
+}
+
+# Main execution
+download_bad_ips
+setup_blacklist
+apply_blacklist
+
+echo "Bad IPs blocking completed."
+
 exit 0
