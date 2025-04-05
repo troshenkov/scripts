494ed8ee67c4b8f1002347c68fd020f84f133ce9
Dmitry Troshenkov <troshenkov.d@gmail.com>
2025-04-04 19:55:10 -0700

    Refactor GeoIP firewall script: modularize functions, improve readability, and error handling

--- iptables/iptab-geo.sh
+++ iptables/iptab-geo.sh
@@ -1,74 +1,85 @@
 #!/bin/bash
-
-# Dmitry Troshenkov (troshenkov.d@gmail.com)
-
-SERVER_IP=`ifconfig eth0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
-PUB_IF=eth0
-
-cd /usr/share/xt_geoip/
-./xt_geoip_dl
-./xt_geoip_build *.csv > INFO
-
-IPT=/sbin/iptables
-#echo ""
-#echo -n "Applying DROP list to existing firewall..."
-$IPT -D INPUT -j GEO
-$IPT -D OUTPUT -j GEO
-$IPT -D FORWARD -j GEO
-$IPT -F GEO
-$IPT -X GEO
-$IPT -N GEO
-
+# ===================================================================
+# GeoIP-based Firewall Rules for Blocking/Allowing Traffic
+# ===================================================================
 #
-# AF – Africa
-# AS – Asia
-# EU – Europe
-# NA – North America
-# OC – Oceania
-# SA – South America
-# A1 – an anonymous proxy
-# A2 – a satellite provider
+# This script updates iptables rules based on GeoIP information,
+# blocking or allowing traffic based on country of origin.
 #
-#_C="CN,ID,IN,PH,PK,SG,TH,AF,SA"
-_CO1="FI,EE,LT,LV,NO,SE,PL,CZ,DE,FR,LU,NL,DK"
-_CO2="RU,MD,US,UA"
+# Author: Dmitry Troshenkov (troshenkov.d@gmail.com)
+# ===================================================================
 
-#$IPT -A GEO  -j LOG --log-prefix "DROP GEO Block"
-#$IPT -A GEO -m geoip --src-cc $_C -j DROP
+# Configuration
+PUB_IF=eth0                        # Public interface for traffic filtering
+SERVER_IP=$(ifconfig eth0 | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1 }')
+XT_GEOIP_DIR="/usr/share/xt_geoip"  # Path to the xt_geoip tool directory
+IPT="/sbin/iptables"               # Path to iptables binary
 
-# Anonymous proxy block
-$IPT -A GEO -m geoip --src-cc A1 -j REJECT
-# Protect HTTP for non RU traffic
-# SO SLOW!! $IPT -I GEO -d ${SERVER_IP} -p tcp --dport 80 ! -i lo -m geoip ! --src-cc $_CO -m string --string 'POST /' --algo bm -j DROP
-#$IPT -I GEO -d ${SERVER_IP} -p tcp --dport 80 ! -i lo -m geoip ! --src-cc $_CO1 -m string --string '/administrator' --algo bm -j DROP
-#$IPT -I GEO -d ${SERVER_IP} -p tcp --dport 80 ! -i lo -m geoip ! --src-cc $_CO1 -m string --string '/bitrix' --algo bm -j DROP
-#$IPT -I GEO -d ${SERVER_IP} -p tcp --dport 80 ! -i lo -m geoip ! --src-cc $_CO2 -m string --string '/administrator' --algo bm -j DROP
-#$IPT -I GEO -d ${SERVER_IP} -p tcp --dport 80 ! -i lo -m geoip ! --src-cc $_CO2 -m string --string '/bitrix' --algo bm -j DROP
-#
-#	Allow HTTP for the China
-$IPT -I GEO -d ${SERVER_IP} -p tcp ! --dport 80 ! -i lo -m geoip --src-cc CN,HK -j DROP
-#
-iptables -A GEO -m geoip --source-country PK,IN,BR,EG,PL,TH,VN,ID,AR,MX,KR,TR,RO -j REJECT
-iptables -A GEO -m geoip --source-country PH,DZ,IR,MA,MY,PE,RS,SA,JP,SG,IL,CL,NG,TW -j REJECT
-iptables -A GEO -m geoip --source-country CO,AR,JO,SN,NP,IN,UG,LA,SA,LY,MG,JM,SD,DO,TO -j REJECT
-iptables -A GEO -m geoip --source-country CR,KW,FJ,SN,NI,HN,EC,PS,CL,ZA,VN,BO -j REJECT
-#
-# ALLOW UKRAINE TO FTP
-$IPT -A GEO -i ${PUB_IF} -p tcp --sport 1024:65535 -d ${SERVER_IP} --dport 21 -m geoip --src-cc UA -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A GEO -o ${PUB_IF} -p tcp -s ${SERVER_IP} --sport 21 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-#
-# Allow Cpanel
-$IPT -A GEO -i ${PUB_IF} -p tcp --sport 1024:65535 -d ${SERVER_IP} --dport 2083 -m geoip --src-cc RU -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A GEO -o ${PUB_IF} -p tcp -s ${SERVER_IP} --sport 2083 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
-# Allow SSH
-$IPT -A GEO -i ${PUB_IF} -p tcp --sport 1024:65535 -d ${SERVER_IP} --dport 22 -m geoip --src-cc RU -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A GEO -o ${PUB_IF} -p tcp -s ${SERVER_IP} --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+# GeoIP Block Categories
+BLOCKED_COUNTRIES="PK,IN,BR,EG,PL,TH,VN,ID,AR,MX,KR,TR,RO"
+ADDITIONAL_BLOCKED_COUNTRIES="PH,DZ,IR,MA,MY,PE,RS,SA,JP,SG,IL,CL,NG,TW"
+EXTRA_BLOCKED_COUNTRIES="CO,AR,JO,SN,NP,IN,UG,LA,SA,LY,MG,JM,SD,DO,TO"
+MORE_BLOCKED_COUNTRIES="CR,KW,FJ,SN,NI,HN,EC,PS,CL,ZA,VN,BO"
+ALLOWED_COUNTRIES="UA,RU,CN,HK"
 
-$IPT -I INPUT -j GEO
-$IPT -I OUTPUT -j GEO
-$IPT -I FORWARD -j GEO
-#echo "...Done"
+# Function to download and build GeoIP data
+update_geoip_data() {
+    echo "Updating GeoIP data..."
+    cd $XT_GEOIP_DIR
+    ./xt_geoip_dl
+    ./xt_geoip_build *.csv > INFO
+}
+
+# Function to initialize iptables chain if it doesn't exist
+initialize_iptables_chain() {
+    echo "Initializing iptables chain '$IPCHAIN'..."
+    if ! iptables -L GEO > /dev/null 2>&1; then
+        iptables -N GEO
+        iptables -I INPUT -j GEO
+        iptables -I OUTPUT -j GEO
+        iptables -I FORWARD -j GEO
+    fi
+}
+
+# Function to flush and reset iptables GEO chain
+reset_iptables_chain() {
+    echo "Flushing and resetting GEO chain..."
+    iptables -D INPUT -j GEO
+    iptables -D OUTPUT -j GEO
+    iptables -D FORWARD -j GEO
+    iptables -F GEO
+    iptables -X GEO
+    iptables -N GEO
+}
+
+# Function to apply GeoIP-based rules to iptables
+apply_geoip_rules() {
+    echo "Applying GeoIP rules..."
+
+    # Block traffic from specific countries
+    iptables -A GEO -m geoip --source-country $BLOCKED_COUNTRIES -j REJECT
+    iptables -A GEO -m geoip --source-country $ADDITIONAL_BLOCKED_COUNTRIES -j REJECT
+    iptables -A GEO -m geoip --source-country $EXTRA_BLOCKED_COUNTRIES -j REJECT
+    iptables -A GEO -m geoip --source-country $MORE_BLOCKED_COUNTRIES -j REJECT
+
+    # Allow traffic from specific countries (e.g., Ukraine for FTP)
+    iptables -A GEO -i $PUB_IF -p tcp --sport 1024:65535 -d $SERVER_IP --dport 21 -m geoip --src-cc UA -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A GEO -o $PUB_IF -p tcp -s $SERVER_IP --sport 21 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+
+    # Allow Cpanel and SSH from Russia
+    iptables -A GEO -i $PUB_IF -p tcp --sport 1024:65535 -d $SERVER_IP --dport 2083 -m geoip --src-cc RU -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A GEO -o $PUB_IF -p tcp -s $SERVER_IP --sport 2083 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+
+    iptables -A GEO -i $PUB_IF -p tcp --sport 1024:65535 -d $SERVER_IP --dport 22 -m geoip --src-cc RU -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A GEO -o $PUB_IF -p tcp -s $SERVER_IP --sport 22 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+}
+
+# Main Execution
+update_geoip_data
+initialize_iptables_chain
+reset_iptables_chain
+apply_geoip_rules
+
+echo "GeoIP firewall rules applied successfully."
 
 exit 0
-
-
