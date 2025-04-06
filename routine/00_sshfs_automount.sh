e9778c35c34938c12d90701ae85f1235bde4bfc4
Dmitry Troshenkov <troshenkov.d@gmail.com>
2025-04-06 09:04:02 -0700

    Improve SSHFS automount script: use /proc/mounts, replace egrep with awk, optimize loops, and add fusermount check

--- routine/00_sshfs_automount.sh
+++ routine/00_sshfs_automount.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # /etc/pm/sleep.d/00_sshfs_automount.sh
+# Handles SSHFS mount/unmount on system suspend and resume
 
-# Unmount all sshfs mountpoints before suspending/hibernating
-# to prevent hanging sshfs after resume/thaw
+case "$1" in
+    hibernate | suspend)
+        # Unmount all SSHFS mountpoints before suspending/hibernating
+        mapfile -t mountpoints < <(awk '$3 == "fuse.sshfs" {print $2}' /proc/mounts)
+        
+        if [ ${#mountpoints[@]} -gt 0 ]; then
+            for mount in "${mountpoints[@]}"; do
+                if command -v fusermount >/dev/null 2>&1; then
+                    fusermount -u "$mount"
+                else
+                    echo "fusermount not found! Cannot unmount $mount."
+                fi
+            done
+        fi
+        ;;
+    thaw | resume)
+        # Re-mount all filesystems
+        mount -a
+        ;;
+esac
 
-case $1 in
-hibernate | suspend)
-mountpoint_count=$(egrep -c '(/[^ ]+) fuse.sshfs' /etc/mtab)
-	if [ $mountpoint_count -gt 0 ]; then
-		mountpoints=($(awk '/(\/[^ ]+) fuse.sshfs/ { print $2 }' /etc/mtab))
-		for element in  $(seq 0 $((${#mountpoints[@]} - 1)))
-		do
-			fusermount -u "${mountpoints[$element]}"
-		done
-	fi
-;;
-	thaw | resume )
-		mount -a
-		;;
-	#   exit 0
-	#   ;;
-	esac
 exit 0
