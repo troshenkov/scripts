b2aac253b43c71075e489f0a4c11718072047d2b
Dmitry Troshenkov <troshenkov.d@gmail.com>
2025-04-13 00:54:43 -0700

    refactor: improve FizzBuzz.sh script readability and efficiency by optimizing control flow and output logic

--- interview/FizzBuzz.sh
+++ interview/FizzBuzz.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+# Script: fizzbuzz.sh
+#
+# Description:
+#   This script prints the numbers from 1 to 100 with the classic FizzBuzz logic:
+#   - For multiples of 3, it prints "Fizz".
+#   - For multiples of 5, it prints "Buzz".
+#   - For numbers divisible by both 3 and 5, it prints "FizzBuzz".
+#   - Otherwise, it simply prints the number.
+#
+# Requirements:
+#   - Bash shell
+#
+# Usage:
+#   Run the script directly:
+#       ./fizzbuzz.sh
+#
+# Author: Dmitry Troshenkov (troshenkov.d@gmail.com)
+# Date: April 13, 2025
+#
+# -----------------------------------------------------------------------------
+# Notes:
+#   - This is a standard coding exercise to test basic control flow and arithmetic.
+#   - Output includes the number followed by its FizzBuzz result, if applicable.
+# -----------------------------------------------------------------------------
+
 COUNTER=1
 
-while [  $COUNTER -lt 101 ]; do
-    if [ $(( $COUNTER % 15 )) -eq 0 ]; then
-        echo "$COUNTER: FizzBizz"
-    elif [ $(( $COUNTER % 3 )) -eq 0 ]; then
+while [ $COUNTER -le 100 ]; do
+    if (( COUNTER % 15 == 0 )); then
+        echo "$COUNTER: FizzBuzz"
+    elif (( COUNTER % 3 == 0 )); then
         echo "$COUNTER: Fizz"
-    elif [ $(( $COUNTER % 5 )) -eq 0 ]; then
+    elif (( COUNTER % 5 == 0 )); then
         echo "$COUNTER: Buzz"
     fi
-    COUNTER=$(( $COUNTER + 1 ))
+    (( COUNTER++ ))
 done
